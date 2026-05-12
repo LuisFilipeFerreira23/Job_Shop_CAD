94bb4868f18813fe430b280a253e303c2b38522f
CyanMango377 <andr.sousasilva@outlook.com>
2026-05-12 21:36:17 +0100

    Code clean

--- Aux_Functions/AuxFunctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef READ_FILE_H
-#define READ_FILE_H
-
-int readFiles(const char *fileName, char *finalString);
-void fileToArray(const char *finalString, int numJobs, int numMachines,
-                 int dataArray[][2 * numMachines]);
-void generateData(int numJobs, int numMachines);
-
-#endif
